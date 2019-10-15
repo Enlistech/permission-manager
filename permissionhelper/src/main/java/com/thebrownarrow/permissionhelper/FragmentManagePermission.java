c950092a442b751224bb3559a879e67fbb636256
Jaydip Umaretiya <jaydip@enlistech.com>
2019-10-15 13:45:55 +0530

    update project with latest lib and versions
    We unpublish the lib for now and we will going to live soon

--- permissionhelper/src/main/java/com/thebrownarrow/permissionhelper/FragmentManagePermission.java
+++ permissionhelper/src/main/java/com/thebrownarrow/permissionhelper/FragmentManagePermission.java
@@ -30,11 +30,10 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
